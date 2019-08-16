29845b535b7bbb7ceb24377f793f75bdbb3ae1ba
Pedro Henrique Rodrigues <pedrohenriquerls@gmail.com>
2019-08-16 17:22:05 -0300

    Simple example to use tabs

--- app/renderer/components/Index.js
+++ app/renderer/components/Index.js
@@ -6,6 +6,102 @@ import Card from '@material-ui/core/Card';
 import Container from '@material-ui/core/Container';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardMedia from '@material-ui/core/CardMedia';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: '100%',
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+function VerticalTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  return (
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+        >
+        <Tab icon={        <ItemCard/>} />
+        <Tab label="Item Three" />
+        <Tab label="Item Four" />
+        <Tab label="Item Five" />
+        <Tab label="Item Six" />
+        <Tab label="Item Seven" />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        Item One
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Item Six
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+        Item Seven
+      </TabPanel>
+    </div>
+  );
+}
 
 const cardStyles = makeStyles(theme => ({
   card: {
@@ -20,17 +116,14 @@ const cardStyles = makeStyles(theme => ({
 function ItemCard() {
   const classes = cardStyles();
   return (
-    <Card className={classes.card} >
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Slack"
-          height="60"
-          image="https://upload.wikimedia.org/wikipedia/commons/7/76/Slack_Icon.png"
-          title="Slack"
-        />
-      </CardActionArea>
-    </Card>
+    <CardMedia
+      className={classes.card}
+      component="img"
+      alt="Slack"
+      height="60"
+      image="https://upload.wikimedia.org/wikipedia/commons/7/76/Slack_Icon.png"
+      title="Slack"
+      />
   );
 }
 
@@ -68,7 +161,7 @@ function Layout() {
         <ItemCard/>
       </Grid>
       <Grid item xs className={classes.content}>
-
+        <VerticalTabs/>
       </Grid>
     </Grid>
   )
