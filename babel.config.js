608d5568373dd72648bcfc6ef5a9984dd4d3aa38
Pedro Rodrigues <pedrohenriquerls@gmail.com>
2019-11-17 01:03:23 -0300

    Add forgotted dependency

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,7 @@ module.exports = {
     '@babel/preset-react',
   ],
   plugins: [
+    "@babel/plugin-proposal-object-rest-spread",
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
